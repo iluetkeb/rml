0bcec73703b61fab1ee6ecf6fe221e83186cc46f
Ingo Luetkebohle <iluetkeb@techfak.uni-bielefeld.de>
2011-09-27 18:48:33 +0200

    fix pathname manipulation errors

--- src/python/rml/session.py
+++ src/python/rml/session.py
@@ -9,22 +9,25 @@ class RMLStateException(Exception):
 		return repr(value)
 
 class RMLDir:
-	INITIAL_CONFIG = dict(last_session=0, last_run=0)
-	CONFIG_NAME = "config"
+	INITIAL_CONFIG = dict(rml_cfg=dict(last_session=0, last_run=0))
+	CONFIG_NAME = "config.json"
 	DIR_NAME = ".rml"
 	def __init__(self,target=os.getcwd()):
-		self.rmldir = os.path.join(os.path.abspath(target), RMLDir.DIR_NAME)
-		self.config = json.load(file(os.path.join(self.rmldir, RMLDir.CONFIG_NAME))
+		self.rmldir = os.path.join(target, RMLDir.DIR_NAME)
+		self.config = json.load(file(os.path.join(self.rmldir, RMLDir.CONFIG_NAME)))
 
 def initialize(target=os.getcwd()):
 	"""If not yet initialized, initialize RMLDir and return it."""
 	rmldir = os.path.join(target, RMLDir.DIR_NAME)
-	if os.path.isdir(rmldir):
-		return RMLDir(rmldir)
+	config_path =os.path.join(rmldir, RMLDir.CONFIG_NAME) 
+	if os.path.isdir(rmldir) and os.path.exists(config_path):
+		return RMLDir(target)
 	else:
-		os.makedirs(rmldir)
-		config = file(os.path.join(rmldir, RMLDir.CONFIG_NAME))
+		if not os.path.isdir(rmldir):
+			os.makedirs(rmldir)
+		print config_path
+		config = file(config_path, "w")
 		json.dump(RMLDir.INITIAL_CONFIG, config)
 		config.close()
-		return RMLDir(rmldir)
+		return RMLDir(target)
 	
