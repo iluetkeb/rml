1519675bb34dddeb5969d17ed22eccce9017717e
Ingo Luetkebohle <iluetkeb@techfak.uni-bielefeld.de>
2011-09-27 19:30:51 +0200

    refactor configuration handling to use common output file name

--- src/python/rml/probes/xcf/event.py
+++ src/python/rml/probes/xcf/event.py
@@ -7,10 +7,10 @@ class XCFProbe(Probe):
 		Probe.__init__(self)
 		self.env = env
 		self.cfg = cfg
-		if not cfg.has_key("xcflog"):
-			raise ProbeConfigurationException("Required configuration item 'xcflog' missing in %s" % repr(cfg))
+		if not cfg.has_key("outputfile"):
+			raise ProbeConfigurationException("Required configuration item 'outputfile' missing in %s" % repr(cfg))
 		self.proc = None
-		self.logfilename = cfg['xcflog']
+		self.logfilename = cfg['outputfile']
 
 	def do_start(self):
 		cmd = ["java", "-cp", "%s:%s/probes/xcf/dist/EventDataLogger.jar" % (self.env.get_classpath(), self.env.get_javabase()), "de.unibi.agai.events.log.LogAll", self.logfilename ]
