111ef30ee439943c84eb3a6b69b0332e215b966a
Ingo Luetkebohle <iluetkeb@techfak.uni-bielefeld.de>
2011-09-28 14:29:00 +0200

    Give some docs

--- src/shell/capture/linux-audio-tcp.sh
+++ src/shell/capture/linux-audio-tcp.sh
@@ -3,8 +3,14 @@
 # Start this on the host that your sound-card is attached to, to enable receiption by the
 # gstreamer audio probe.
 #
-# If you modify this pipeline to accomodate other clients, make sure to keep the 'protocol=1'
-# part, the receiver needs it for caps negotiation.
+# Synopsis: ./linux-audio-tcp.sh [device]
+# [device] is an ALSA-Style sound device specification, e.g. "plughw:1" for the second 
+# capture-capable audio-device present in the system. See "arecord -l" for the appropriate
+# numbers
+#
+# DEVELOPERS NOTE: If you modify this pipeline to accomodate other clients, make sure 
+# to keep the 'protocol=1' part, the receiver needs it for caps negotiation.
+# 
 
 if [ "x$1" != "x" ]
 then
