fe9071ca0eed60f000c26eeb81304cecf494fe87
Ingo Luetkebohle <iluetkeb@techfak.uni-bielefeld.de>
2011-10-30 13:25:04 +0100

    support type-specifiers for check_keys

--- src/python/rml/config.py
+++ src/python/rml/config.py
@@ -13,7 +13,7 @@ class Configuration:
 
 	>>> from StringIO import StringIO
 	>>> c = Configuration(StringIO(json.dumps(Configuration.INITIAL_CONFIG)))
-	>>> c = Configuration(StringIO(json.dumps({'rml_cfg': { 'probes': { 'test': { 'class': 'event', 'type': 'XCF', 'location': 'xcflog.xml' }}}})))
+	>>> c = Configuration(StringIO(json.dumps({'rml_cfg': { 'version': 0.3, 'probes': { 'test': { 'class': 'event', 'type': 'XCF', 'location': 'xcflog.xml' }}}})))
 	>>> len(c.get_probe_cfgs())
 	1
 	'''
@@ -45,8 +45,13 @@ class Configuration:
 		self.base_config = json.load(conf_input)		
 		self.rml_config = self.base_config[self.__KEY_BASE]
 		if not self.rml_config.has_key(self.__KEY_VERSION):
+			name = None
+			if hasattr(conf_input, 'name'):
+				name = conf_input.name
+			else:
+				name = conf_input.getvalue()
 			raise ConfigurationException("Configuration '%s' missing version information." %
-				conf_input.name)
+				name)
 		if self.rml_config[self.__KEY_VERSION] < 0.3:
 			raise ConfigurationException("Versions prior to 0.3 not supported anymore.")
 
@@ -65,12 +70,19 @@ class Configuration:
 	def get_probe_cfgs(self):
 		return self.probe_cfgs
 
+from types import *
+
 class ProbeConfiguration:
+	__typemap = {'int': IntType, 'boolean': BooleanType, 'long': LongType, 'float': FloatType,
+	'str': StringType, 'strs': StringTypes, 'unicode': UnicodeType, 'tuple': TupleType,
+	'list': ListType, 'map': DictType }
+
+
 	'''
 	Configuration class for probes.
 
-	>>> c = ProbeConfiguration(None, {'class': 'event', 'type': 'test', 'location': 'foo.xml', 'option': 'arg'})
-	>>> c.check_keys(['location', 'option'])
+	>>> c = ProbeConfiguration(None, {'class': 'event', 'type': 'test', 'location': 'foo.xml', 'option': 'arg', 'num': 1})
+	>>> c.check_keys(['location', 'option', 'num:int'])
 	>>> c.get_outputlocation()
 	'foo.xml'
 	>>> c.get_category()
@@ -91,13 +103,34 @@ class ProbeConfiguration:
 		self.check_keys([self.__KEY_LOC])
 		
 	def check_keys(self, keys):
+		'''Check that all keys in the specified list are present as configuration variables.
+		The list of names may contain embedded type-specifiers, seperated by ':',
+		like so: count:int. Multiple type-specifiers can be given, separated by ','.'''
 		missing = []
+		badtype = []
 		for key in keys:
+			name = key
+			try:
+				name, t = key.split(":", 2)
+				ts = [self.__typemap[s] for s in t.split(",")]
+			except ValueError:
+				ts = StringTypes
+
 			if not self.probe_cfg.has_key(key):
 				missing.append(key)
+			if not isinstance(self.probe_cfg[key], ts):
+				badtype.append((key, ts, type(self.probe_cfg[key])))
+		msg = []		
 		if len(missing) > 0:
-			raise ConfigurationException("Required key(s) '%s' missing in probe_cfg '%s'" %
-				missing, self.probe_cfg)
+			msg.append("Required key(s) '%s' missing in probe_cfg '%s'" %
+				(missing, self.probe_cfg ))
+		if len(badtype) > 0:
+			for bt in badtype:
+				msg.append("Key '%s' had bad type. Expected: '%s', was '%s'" % bt)
+
+		if len(msg) > 0:
+			raise ConfigurationException("\n".join(msg))
+		
 
 	def get_outputlocation(self):
 		return self.probe_cfg[self.__KEY_LOC]
