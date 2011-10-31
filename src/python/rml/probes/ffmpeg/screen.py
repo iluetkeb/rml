63218d5e971723037f6743f3f3e10548bb1c1d51
Ingo Luetkebohle <iluetkeb@techfak.uni-bielefeld.de>
2011-10-31 14:44:11 +0100

    implemented decent screen capture

--- src/python/rml/probes/ffmpeg/screen.py
+++ src/python/rml/probes/ffmpeg/screen.py
@@ -2,12 +2,18 @@ from .. import Probe, ProbeConfigurationException
 
 import subprocess, signal
 
+import gtk.gdk
+
 class ScreenProbe(Probe):
 	__KEY_DISPLAY = "display"
 	__KEY_FPS = "fps"
-	__KEY_OSIZE = "output_size"
+	__KEY_WIDTH = "width"
+	__KEY_HEIGHT = "height"
+	__KEY_SCALE = "scale"
+	__KEY_QUALITY = "quality"
+	__KEY_BITRATE = "bitrate"
 	REQ_CONFIG = [ __KEY_DISPLAY ]
-	OPT_CONFIG = [ __KEY_FPS, __KEY_OSIZE ]
+	OPT_CONFIG = [ "%s:int" % __KEY_FPS, "%s:int" % __KEY_WIDTH, "%s:int" % __KEY_HEIGHT, "%s:int" % __KEY_QUALITY, "%s:strs", "%s:int" % __KEY_SCALE ]
 
 	def __init__(self, env, cfg):
 		Probe.__init__(self)
@@ -18,10 +24,34 @@ class ScreenProbe(Probe):
 		self.capturefile = cfg.get_outputlocation()
 		self.display = cfg.get(self.__KEY_DISPLAY)
 		self.fps = cfg.get(self.__KEY_FPS, 12.5)
-		self.size = cfg.get(self.__KEY_OSIZE, '640x480')
+		display = gtk.gdk.Display(self.display)
+		screen = display.get_default_screen()
+		w = cfg.get(self.__KEY_WIDTH, screen.get_width())
+		h = cfg.get(self.__KEY_HEIGHT, screen.get_height())
+		self.size = "%dx%d" %(w, h)
+		self.quality = cfg.get(self.__KEY_QUALITY, 3)
+		self.scale = cfg.get(self.__KEY_SCALE, 1)
+
+		owidth = w/self.scale
+		oheight = h/self.scale
+		#opix = owidth * oheight
+		#rate_estimate = opix / 50
+		self.osize = "%dx%d" % ( owidth, oheight )
 
 	def do_start(self):
-		cmd = ["ffmpeg", "-s", self.size, "-i", self.display, "-r", self.fps, self.capturefile ]
+		cmd = ["ffmpeg", "-loglevel", "info", "-y", 
+
+			"-f", "x11grab", 
+			"-s", self.size, 
+			"-r", str(self.fps), 
+			"-i", self.display, 
+
+			"-qscale", str(self.quality), 
+			"-vcodec", "libxvid", 
+			"-acodec", "none",
+			"-s", self.osize,
+			self.capturefile 
+		]
 		print cmd
 		self.proc = subprocess.Popen(cmd, bufsize=1)
 		if not self.proc:
@@ -32,7 +62,7 @@ class ScreenProbe(Probe):
 		self.proc.wait()
 
 	def is_alive(self):
-		return Probe.is_alive(self) and self.proc is not None
+		return Probe.is_alive(self) and self.proc is not None and self.proc.returncode is None
 
 	def __repr__(self):
-		return repr(self.logfilename)
+		return repr(self.display)
