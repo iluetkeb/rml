a22eeb0141417281363d3db1fca53f62e92b0510
Ingo Luetkebohle <iluetkeb@techfak.uni-bielefeld.de>
2012-12-18 13:55:30 +0100

    remove distribute, didn't change anything

--- src/python/setup.py
+++ src/python/setup.py
@@ -11,7 +11,6 @@ setup(
 		'gstreamer': 'gst-python>=0.10',
 		'opencv': 'opencv'
 	},
-	install_requires=['distribute'],
 
 	author="Ingo Luetkebohle",
 	author_email="iluetkeb@techfak.uni-bielefeld.de",
