7e121681627cdb4137f1d7a1d95c18285d374b2c
Ingo Luetkebohle <iluetkeb@techfak.uni-bielefeld.de>
2013-04-07 16:21:02 +0200

    remove queue

--- src/python/rml/probes/gstreamer/network_camera.py
+++ src/python/rml/probes/gstreamer/network_camera.py
@@ -38,7 +38,7 @@ class H264NetworkCameraProbe(Probe):
 		if cfg.get(self.__KEY_USER) and cfg.get(self.__KEY_PASSWD):
 			spec += " user-id={user} user-pw={pwd}".format(**args)
 
-		spec += " ! queue name=rtp rtp. ! rtph264depay ! capsfilter caps=\"video/x-h264, width={width}, height={height}, framerate=(fraction){fps}/1\" ! matroskamux name=mux ! filesink location={output} ".format(**args)
+		spec += " name=rtp rtp. ! rtph264depay ! capsfilter caps=\"video/x-h264, width={width}, height={height}, framerate=(fraction){fps}/1\" ! matroskamux name=mux ! filesink location={output} ".format(**args)
 		# add audio, unless disabled
 		if not cfg.get(self.__KEY_NOAUDIO):
 			spec += "  rtp. ! rtpmp4gdepay ! capsfilter caps=\"audio/mpeg, mpegversion=(int)4\" ! faad ! faac ! mux. ".format(**args)
