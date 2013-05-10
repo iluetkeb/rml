55bb9aabc2ce25fc06d0b52ab73ae1942a12c28b
flier <flier@techfak.uni-bielefeld.de>
2013-05-10 19:49:37 +0200

    forgot some probes omg

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     scripts=['src/python/bin/rml'],
-    packages=['rml','rml.probes'],
+    packages=['rml','rml.probes','rml.probes.ros','rml.probes.opencv','rml.probes.lcm','rml.probes.gstreamer','rml.probes.ffmpeg','rml.probes.rsb', 'rml.probes.xcf'],
     package_dir={'': 'src/python'},
     )
 
